3defd0419aeff7f7c46725fdf7bfe41ac0910377
komardev <komarudin2110@gmail.com>
2023-05-24 11:34:41 +0700

    manifest testing

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'React Playground Name',
+        short_name: 'React Playground Short',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'fullscreen',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') }
+  }
+})
